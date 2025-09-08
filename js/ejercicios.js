62e54af39645d87bccb4866a04827d8f45e7df36
TOMAS ARIEL GOLDENTUL <49319223@ort>
2025-09-08 09:04:22 -0300

    Finalizado la logica del programa, comenzando CSS

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,12 +1,18 @@
 document.getElementById('resultado')
 document.getElementById('resultadoFrutal')
 document.getElementById('resultadoComparaciones')
+document.getElementById('cortarTextoVer');
 
-function calcularEdad(){
-    const fechaOriginal = document.getElementById('edad').value;
-    const diaHoy = obtenerFecha();
-    resultado.innerHTML = (Date(diaHoy) - Date(fechaOriginal));
+function calcularEdad() {
+    const fechaOriginalStr = document.getElementById('edad').value;
+    const fechaOriginal = new Date(fechaOriginalStr);
+    const hoy = new Date();
+
+    const edad = hoy.getFullYear() - fechaOriginal.getFullYear();
+
+    console.log("Edad:", edad);
 }
+
 function mostrarFrutas(){
     const frutas = vectorFrutas();
     for(fruta of frutas){
@@ -108,9 +114,35 @@ function reemplazarPalabras(){
     remplazar.innerHTML = resultado;
 }
 
-function cortarTexto(){
-    const cadena = document.getElementById('cadena').value;
+function cortarTexto() {
+    const cadena = document.getElementById('cadenaCortar').value;
     const numero = parseInt(document.getElementById('numero').value);
-    const cadenaNueva = cadena.slice(0, numero)
-    console.log(cadenaNueva)
+    
+    const cadenaNueva = cadena.slice(0, numero);
+    document.getElementById('cortarTextoVer').innerText = cadenaNueva;
+}
+
+function separadorTexto() {
+    const cadenaComas = document.getElementById('cadenaComas').value;
+    const newCadenaComas = cadenaComas.split(',');
+    const resultadoSeparador = newCadenaComas.join('-');
+
+    const stringSeparados = document.getElementById('stringSeparados');
+    stringSeparados.innerHTML = resultadoSeparador;
+}
+
+function recaudacion() {
+    const cadenaRecaudo = document.getElementById('cadenaRecaudo').value.trim();
+    const newCadenaRecaudo = cadenaRecaudo.split(',');
+    const newNewCadenaRecaudo = newCadenaRecaudo.map(obj => obj.split(':'));
+    
+    let resultadoRecaudo = 0;
+
+    for (let i = 0; i < newNewCadenaRecaudo.length; i++) {
+        const value = Number(newNewCadenaRecaudo[i][1].trim());
+        resultadoRecaudo += value;
+
+    }
+    
+    document.getElementById('recaudacion').innerHTML = resultadoRecaudo;
 }
